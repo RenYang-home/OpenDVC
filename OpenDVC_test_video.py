6be4b38ca6efa799d88ce93e6d0b7eeb451831dc
Ren Yang <r.yangchn@gmail.com>
2020-06-26 13:36:27 +0200

    Add files via upload

--- OpenDVC_test_video.py
+++ OpenDVC_test_video.py
@@ -7,6 +7,7 @@ import CNN_img
 import motion
 import MC_network
 import os
+from ms_ssim_np import MultiScaleSSIM
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -18,13 +19,15 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--path", default='BasketballPass')
 parser.add_argument("--frame", type=int, default=100)
 parser.add_argument("--GOP", type=int, default=10)
-parser.add_argument("--l", type=int, default=1024)
-parser.add_argument("--N", type=int, default=128)
-parser.add_argument("--M", type=int, default=128)
+parser.add_argument("--mode", default='PSNR', choices=['PSNR', 'MS-SSIM'])
+parser.add_argument("--metric", default='MS-SSIM', choices=['PSNR', 'MS-SSIM'])
+parser.add_argument("--python_path", default='/scratch_net/maja_second/Demo/bin/python')
+parser.add_argument("--Lee_path", default='/scratch_net/maja_second/CA_Entropy_Model-master/CA_EntropyModel_Test/')
+parser.add_argument("--l", type=int, default=1024, choices=[8, 16, 32, 64, 256, 512, 1024, 2048])
+parser.add_argument("--N", type=int, default=128, choices=[128])
+parser.add_argument("--M", type=int, default=128, choices=[128])
 
 args = parser.parse_args()
-
-assert args.l == 256 or args.l == 512 or args.l == 1024 or 2048
 
 if args.l == 256:
     I_QP = 37
@@ -35,8 +38,18 @@ elif args.l == 1024:
 elif args.l == 2048:
     I_QP = 22
 
-path_com = './' + args.path + '_com_' + str(args.l) + '/'
-path_bin = './' + args.path + '_bin_' + str(args.l) + '/'
+elif args.l == 8:
+    I_level = 2
+elif args.l == 16:
+    I_level = 3
+elif args.l == 32:
+    I_level = 5
+elif args.l == 64:
+    I_level = 7
+
+path = '/scratch_net/maja_second/HEVC_sequences/test_raw/' + args.path + '/'
+path_com = './' + args.path + '_com_' + args.mode  + '_' + str(args.l) + '/'
+path_bin = './' + args.path + '_bin_' + args.mode  + '_' + str(args.l) + '/'
 
 os.makedirs(path_com, exist_ok=True)
 os.makedirs(path_bin, exist_ok=True)
@@ -92,44 +105,61 @@ Res_hat = CNN_img.Res_synthesis(y_hat, num_filters=args.N)
 # Reconstructed frame
 Y1_com = tf.clip_by_value(Res_hat + Y1_MC, 0, 1)
 
-train_mse = tf.reduce_mean(tf.squared_difference(Y1_com, Y1_raw))
-psnr = 10.0*tf.log(1.0/train_mse)/tf.log(10.0)
+if args.metric == 'PSNR':
+    train_mse = tf.reduce_mean(tf.squared_difference(Y1_com, Y1_raw))
+    quality = 10.0*tf.log(1.0/train_mse)/tf.log(10.0)
+elif args.metric == 'MS-SSIM':
+    quality = tf.math.reduce_mean(tf.image.ssim_multiscale(Y1_com, Y1_raw, max_val=1))
 
 saver = tf.train.Saver(max_to_keep=None)
-model_path = './PSNR_' + str(args.l) + '_model/model.ckpt'
+model_path = './' + args.mode + '_' + str(args.l) + '_model/model.ckpt'
 saver.restore(sess, save_path=model_path)
 
-psnr_frame = np.zeros([args.frame])
+quality_frame = np.zeros([args.frame])
 bits_frame = np.zeros([args.frame])
 
 for gop_number in range(np.int(np.ceil(args.frame/args.GOP))):
 
     f = gop_number * args.GOP
 
-    os.system('bpgenc -f 444 -m 9 ' + path + 'f' + str(f + 1).zfill(3) + '.png -o ' + path_bin + str(f + 1).zfill(3) + '.bin -q ' + str(I_QP))
-    os.system('bpgdec ' + path_bin + str(f + 1).zfill(3) + '.bin -o ' + path_com + 'f' + str(f + 1).zfill(3) + '.png')
+    if args.mode == 'PSNR':
+        os.system('bpgenc -f 444 -m 9 ' + path + 'f' + str(f + 1).zfill(3) + '.png -o ' + path_bin + str(f + 1).zfill(3) + '.bin -q ' + str(I_QP))
+        os.system('bpgdec ' + path_bin + str(f + 1).zfill(3) + '.bin -o ' + path_com + 'f' + str(f + 1).zfill(3) + '.png')
+    elif args.mode == 'MS-SSIM':
+        os.system(args.python_path + ' ' + args.Lee_path + '/encode.py --model_type 1 --input_path ' + path + 'f' + str(f + 1).zfill(3) + '.png' +
+                  ' --compressed_file_path ' + path_bin + str(f + 1).zfill(3) + '.bin' + ' --quality_level ' + str(I_level))
+        os.system(args.python_path + ' ' + args.Lee_path + '/decode.py --compressed_file_path ' + path_bin + str(f + 1).zfill(3) + '.bin'
+                  + ' --recon_path ' + path_com + 'f' + str(f + 1).zfill(3) + '.png')
+
     bits = os.path.getsize(path_bin + str(f + 1).zfill(3) + '.bin')
     bits = bits * 8
 
     F0_com = misc.imread(path_com + 'f' + str(f + 1).zfill(3) + '.png')
     F0_raw = misc.imread(path + 'f' + str(f + 1).zfill(3) + '.png')
-    mse = np.mean(np.power(np.subtract(F0_com / 255.0, F0_raw / 255.0), 2.0))
-
-    psnr_frame[f] = 10 * np.log10(1.0 / mse)
-    bits_frame[f] = bits
-    print('Frame', f + 1, 'PSNR = ', psnr_frame[f], 'bpp = ', bits_frame[f] / Height / Width)
 
     F0_com = np.expand_dims(F0_com, axis=0)
+    F0_raw = np.expand_dims(F0_raw, axis=0)
 
-    for p_frame in range(args.GOP - 1):
+    if args.metric == 'PSNR':
+        mse = np.mean(np.power(np.subtract(F0_com / 255.0, F0_raw / 255.0), 2.0))
+        quality_frame[f] = 10 * np.log10(1.0 / mse)
+    elif args.metric == 'MS-SSIM':
+        quality_frame[f] = MultiScaleSSIM(F0_com, F0_raw, max_val=255)
+
+    bits_frame[f] = bits
+    print('Frame', f + 1, args.metric + ' =', quality_frame[f], 'bpp =', bits_frame[f] / Height / Width)
+
+    p_frame_number = np.min([args.GOP - 1, args.frame - f - 1])
+
+    for p_frame in range(p_frame_number):
 
         f = f + 1
 
         F1_raw = misc.imread(path + 'f' + str(f + 1).zfill(3) + '.png')
         F1_raw = np.expand_dims(F1_raw, axis=0)
 
-        F0_com, string_MV, string_Res, psnr_com \
-            = sess.run([Y1_com, string_mv, string_res, psnr],
+        F0_com, string_MV, string_Res, quality_com \
+            = sess.run([Y1_com, string_mv, string_res, quality],
                        feed_dict={Y0_com: F0_com / 255.0, Y1_raw: F1_raw / 255.0})
 
         F0_com = F0_com * 255.0
@@ -144,12 +174,15 @@ for gop_number in range(np.int(np.ceil(args.frame/args.GOP))):
 
         bits = (2 + len(string_MV) + len(string_Res)) * 8
 
-        psnr_frame[f] = psnr_com
+        quality_frame[f] = quality_com
         bits_frame[f] = bits
 
-        print('Frame', f + 1, 'PSNR = ', psnr_frame[f], 'bpp = ', bits_frame[f] / Height / Width)
+        print('Frame', f + 1, args.metric + ' =', quality_frame[f], 'bpp =', bits_frame[f] / Height / Width)
 
-psnr_ave = np.average(psnr_frame)
+quality_ave = np.average(quality_frame)
 bits_ave = np.average(bits_frame / Height / Width)
 
-print('Average PSNR = ', psnr_ave, 'Average bpp = ', bits_ave)
+print('Average ' + args.metric + ' =', quality_ave, 'Average bpp =', bits_ave)
+
+file = open("./results.txt", "a")
+file.write(args.path + ' ' + str(args.l) + ' bpp = ' + str(bits_ave) + ' ssim = ' + str(quality_ave) + '\n')
